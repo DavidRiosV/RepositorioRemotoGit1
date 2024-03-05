195c44ca0351b47247d33ae8d6ff03171a21300a
alumnado <alumnado@A120-pc07>
2024-03-05 13:36:45 +0100

    V1

--- Proyecto1/src/paquete1/principal.java
+++ Proyecto1/src/paquete1/principal.java
@@ -3,7 +3,9 @@ package paquete1;
 public class principal {
 
 	public static void main(String[] args) {
-		System.out.print("Hola");
-		System.out.print("Adios");
+		System.out.println("Hola");
+		System.out.println("Adios");
+		//Comentario para probar remoto
+		//jujujju
 	}
 }
